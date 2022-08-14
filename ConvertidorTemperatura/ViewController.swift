fd6176be55edd84ed07b072ef837e73cfb670394
Enrique A Sanchez <enrike.sanchez@tec.mx>
2022-08-14 12:35:17 -0500

    first commit

---
+++ ConvertidorTemperatura/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConvertidorTemperatura
+//
+//  Created by user222911 on 8/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
